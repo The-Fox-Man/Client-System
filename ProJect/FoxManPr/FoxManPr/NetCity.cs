c64d36d4b864134b87cd80b4097ec5ea8b927eb2
Artem Lisenkoff <artem.lisenkoff@gmail.ru>
2022-11-28 22:49:03 +0400

    Pleaseeee, take me away from this nightmare This is the really great update of NetCity form and I hope the last update, I got tired, this is the form for puple, so let's go, we have another things to do!

--- ProJect/FoxManPr/FoxManPr/NetCity.cs
+++ ProJect/FoxManPr/FoxManPr/NetCity.cs
@@ -41,11 +41,11 @@ namespace FoxManPr
 
         Label lbl1;
 
-        int dayS = 01;
-        int monS = 09;
+        int dayS = 29;
+        int monS = 08;
         int yeaS = 2022;
 
-        int dayE = 06;
+        int dayE = 03;
         int monE = 09;
         int yeaE = 2022;
 
@@ -99,21 +99,54 @@ namespace FoxManPr
                 tbl.Controls.Add(lbl1, 3, Convert.ToInt32(mark[i + 2]) - 1);
             }
         }
+
+
+
+        int mon = 8;
+        int year = 2022;
+
+        private void StopIt(int i, int k, Label one)
+        {
+
+            if (monS == 9 && i > 30) { mon = 10; one.Text = i - 30 + "." + mon + "." + year; }
+            else if (monS == 10 && i > 31) { mon = 11; one.Text = i - 31 + "." + mon + "." + year; }
+            else if (monS == 11 && i > 30) { mon = 12; one.Text = i - 30 + "." + mon + "." + year; }
+            else if (monS == 12 && i > 31) { mon = 1; year = year + 1; one.Text = i - 31 + "." + mon + "." + year; }
+            else if (monS == 1 && i > 31) { mon = 2; one.Text = i - 31 + "." + mon + "." + year; }
+            else if (monS == 2 && i > 28)
+            {
+                if (yeaE == 2020 || yeaE == 2024 || yeaE == 2028 || yeaE == 2032 || yeaE == 2036 || yeaE == 2040 || yeaE == 2044 || yeaE == 2048 || yeaE == 2052 || yeaE == 2056 || yeaE == 2060)
+                { mon = 3; one.Text = i - 29 + "." + mon + "." + year; }
+                else
+                { mon = 3; one.Text = i - 28 + "." + mon + "." + year; }
+            }
+            else if (monS == 3 && i > 31) { mon = 4; one.Text = i - 31 + "." + mon + "." + year; }
+            else if (monS == 4 && i > 30) { mon = 5; one.Text = i - 30 + "." + mon + "." + year; }
+            else if (monS == 5 && i > 31) { mon = 6; one.Text = i - 31 + "." + mon + "." + year; }
+            else if (monS == 6 && i > 30) { mon = 7; one.Text = i - 30 + "." + mon + "." + year; }
+            else if (monS == 7 && i > 31) { mon = 8; one.Text = i - 31 + "." + mon + "." + year; }
+            else if (monS == 8 && i > 31) { mon = 9; one.Text = i - 31 + "." + mon + "." + year; }
+
+            else { one.Text = i + "." + mon + "." + year; }
+        }
+
         public NetCity()
         {
             InitializeComponent();
             user.Text = login.nameForm + " " + login.surnForm;
             number.Controls.Clear();
             //  List<string> subject = MySelect("SELECT 1t, 2d, 3d, 4th, 5th, 6th, 7th FROM subjects");
-            //    List<string> date = MySelect("SELECT date, day");
-            //   List<string> sub = MySelect("SELECT name FROM sub WHERE id = '"+ mark[2] +"'");
-            number.Text = dayS  + "." + monS + "." + yeaS + " - " + dayE + "." + monE + "." + yeaE;
+            //  List<string> date = MySelect("SELECT date, day");
+            //  List<string> sub = MySelect("SELECT name FROM sub WHERE id = '"+ mark[2] +"'");
+            int ant = dayS + 1;  int ant2 = dayS + 2; int ant3 = dayS + 3; int ant4 = dayS + 4;
+
+            number.Text = dayS + "." + monS + "." + yeaS + " - " + dayE + "." + monE + "." + yeaE;
             date1.Text = dayS  + "." + monS + "." + yeaS;
-            date2.Text = dayS + 1 + "." + monS + "." + yeaS;
-            date3.Text = dayS + 2 + "." + monS + "." + yeaS;
-            date4.Text = dayS + 3 + "." + monS + "." + yeaS;
-            date5.Text = dayS + 4 + "." + monS + "." + yeaS;
-            date6.Text = dayS + 5 + "." + monS + "." + yeaS;
+            StopIt(ant, 1, date2);
+            StopIt(ant2, 2, date3);
+            StopIt(ant3, 3, date4);
+            StopIt(ant4, 4, date5);
+            date6.Text = dayE + "." + monE + "." + yeaE;
 
             for (int i = 0; i < subject.Count; i++)
             {
@@ -354,13 +387,18 @@ namespace FoxManPr
             if (monE == 8 && dayE > 31) { dayE = dayE - 31; monE = 9; }
             if (monS == 8 && dayS > 31) { dayS = dayS - 31; monS = 9; }
 
+            int ant = dayS + 1;
+            int ant2 = dayS + 2;
+            int ant3 = dayS + 3;
+            int ant4 = dayS + 4;
+
             number.Text = dayS + "." + monS + "." + yeaS + " - " + dayE + "." + monE + "." + yeaE;
             date1.Text = dayS + "." + monS + "." + yeaS;
-            date2.Text = dayS + 1 + "." + monS + "." + yeaS;
-            date3.Text = dayS + 2 + "." + monS + "." + yeaS;
-            date4.Text = dayS + 3 + "." + monS + "." + yeaS;
-            date5.Text = dayS + 4 + "." + monS + "." + yeaS;
-            date6.Text = dayS + 5 + "." + monS + "." + yeaS;
+            StopIt(ant, 1, date2);
+            StopIt(ant2, 2, date3);
+            StopIt(ant3, 3, date4);
+            StopIt(ant4, 4, date5);
+            date6.Text = dayE + "." + monE + "." + yeaE;
             number.Controls.Clear();
 
             for (int i = 0; i < subject.Count; i++)
@@ -447,7 +485,7 @@ namespace FoxManPr
             if (monS == 12 && dayS == 0) { dayS = 30; monS = monS - 1; }
 
             if (monE == 1 && dayE < 0) { dayE = dayE + 31; monE = 12; yeaE = yeaE - 1; }
-            if (monE == 1 && dayE == 0) { dayE = 31; monE = monE - 1; }
+            if (monE == 1 && dayE == 0) { dayE = 31; monE = monE + 11; yeaE = yeaE - 1; }
             if (monS == 1 && dayS < 0) { dayS = dayS + 31; monS = 12; yeaS = yeaS - 1; }
             if (monS == 1 && dayS == 0) { dayS = 31; monS = monS - 1; }
 
@@ -511,13 +549,18 @@ namespace FoxManPr
             if (monS == 9 && dayS < 0) { dayS = dayS + 31; monS = 8; }
             if (monS == 9 && dayS == 0) { dayS = 31; monS = monS - 1; }
 
+            int ant = dayS + 1;
+            int ant2 = dayS + 2;
+            int ant3 = dayS + 3;
+            int ant4 = dayS + 4;
+
             number.Text = dayS + "." + monS + "." + yeaS + " - " + dayE + "." + monE + "." + yeaE;
             date1.Text = dayS + "." + monS + "." + yeaS;
-            date2.Text = dayS + 1 + "." + monS + "." + yeaS;
-            date3.Text = dayS + 2 + "." + monS + "." + yeaS;
-            date4.Text = dayS + 3 + "." + monS + "." + yeaS;
-            date5.Text = dayS + 4 + "." + monS + "." + yeaS;
-            date6.Text = dayS + 5 + "." + monS + "." + yeaS;
+            StopIt(ant, 1, date2);
+            StopIt(ant2, 2, date3);
+            StopIt(ant3, 3, date4);
+            StopIt(ant4, 4, date5);
+            date6.Text = dayE + "." + monE + "." + yeaE;
             number.Controls.Clear();
 
             for (int i = 0; i < subject.Count; i++)
